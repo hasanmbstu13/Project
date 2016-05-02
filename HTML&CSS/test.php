69c9632f075937686c29b3a89c6f95b51b778aa9
hasanmbstu13 <hasanmbstu13@gmail.com>
2016-05-02 08:06:59 +0600

    Implement a project named pricing_table using html & CSS

---
+++ HTML&CSS/test.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>This page is just for test</title>
+</head>
+<body>
+	<h1>Hello</h1>
+	<h2>World</h2>
+	<h3>How r u?</h3>
+	<h4>I am fine</h4>
+	<h5>And U</h5>
+	<h6>When u Wake up</h6>
+	<h4>Well</h4>
+</body>
+</html>
